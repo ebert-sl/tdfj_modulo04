4306e4738f375d3ae5d332f6e4f2da076983e2f9
ebert-sl <ebertis2002@gmail.com>
2024-07-20 11:57:14 -0300

    initial commit

---
+++ aula34/first_header/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+      <div>
+        <a href="#">Home</a>
+        <a href="#">Seção 1</a>
+        <a href="#">Seção 2</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
